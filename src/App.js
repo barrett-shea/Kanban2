9afa0e300e065ace5ab6d51dd1b713d5adbf46d1
barrett-shea <barrettkshea@gmail.com>
2021-05-26 20:12:16 -0400

    Persist list ordering

--- src/App.js
+++ src/App.js
@@ -5,8 +5,41 @@ import {DragDropContext} from 'react-beautiful-dnd'
 
 
 export default function App () {
-  const onDragEnd = result => {
-    // TODO: reorder column
+  const [order, setOrder] = useState(InitialData)
+  
+  const onDragEnd = result => {     //used to persist new order after drag
+    const {destination, source, draggableId} = result
+
+    if (!destination) {
+      return
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    const column = InitialData.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    }
+
+    const newState = {
+      ...InitialData,
+      columns: {
+        ...InitialData.columns,
+      },
+      [newColumn.id]: newColumn, 
+    }
+
+    setState(newState)
   };
   
   return (
