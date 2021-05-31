97950e3c1f48793ae269c822e17a3f289413a616
barrett-shea <barrettkshea@gmail.com>
2021-05-31 19:14:38 -0400

    Style change- full-width column highlight

--- src/Column.js
+++ src/Column.js
@@ -4,11 +4,11 @@ import {Droppable} from 'react-beautiful-dnd'
 
 export default function Column (props) {
   return (
-    <div className='border rounded p-2 m-2 w-25 d-flex flex-column'>
-      <h1>{props.column.title}</h1>
+    <div className='border rounded m-2 w-25 d-flex flex-column'>
+      <h1 classname='p-2'>{props.column.title}</h1>
       <Droppable droppableId = {props.column.id}>
         {(provided, snapshot) => (
-          <div className={`flex-grow-1 ${snapshot.isDraggingOver ? 'bg-success' : 'bg-white'}`}
+          <div className={`flex-grow-1 p-2 ${snapshot.isDraggingOver ? 'bg-success' : 'bg-white'}`}
           ref={provided.innerRef}
           {...provided.droppableProps}
           isDraggingOver={snapshot.isDraggingOver}
