353126606e603cacc2877b90e32130730ad9a3ce
barrett-shea <barrettkshea@gmail.com>
2021-06-08 00:23:25 -0400

    Add task local state change

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,19 +1,40 @@
 import React, {useState} from "react";
 import {Form, Button, Container} from 'react-bootstrap'
+import InitialData from './InitialData'
 
-export default function AddTask (props) {
+export default function AddTask () {
+  const [state, setState] = useState(InitialData)
   const [value, setValue] = useState('')
-
   function handleChange(e) {
     setValue(e.target.value)
   }
 
   function handleSubmit() {
-    newTask = props.tasks.concat()
-    setState(newTask)
+    console.log(`old state- ${JSON.stringify(state)}`)
+// add task to state.tasks
+    const newId = `task${(Object.keys(state.tasks).length)+1}` 
+    const target = state.tasks
+    const source = {'task5': {id: newId, content: value}} //key needs to be set to variable
+  
+    const newTask = Object.assign(target, source) 
 
-    newList = props.columns.column1.taskIds.concat({value})
-    setState(newList)
+// add newTask to column1
+    const newList = state.columns['column1'].taskIds
+    newList.push(newId)
+    
+    
+    const newState = {
+      ...state.tasks,
+      tasks: newTask,
+      columns: {
+        ...state.columns,
+        ['column1']: newList
+      }
+    }
+
+    setState(newState)
+    console.log(`new state- ${JSON.stringify(state)}`)
+// state not being updated in other components(not being displayed)
 
     setValue('')
   }
