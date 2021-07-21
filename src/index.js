fefb5aa1cc116b1b3fb0ec4ea53f514030a76eb8
barrett-shea <barrettkshea@gmail.com>
2021-07-21 17:37:49 -0400

    App1 Setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App2 from "./Components/App2";
+import App from "./Components/App";
 import "bootstrap/dist/css/bootstrap.min.css"
 
-ReactDOM.render(<App2 />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
