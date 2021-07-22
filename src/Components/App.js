ce252b6c220e7af3e38f7d77a69aacb603bf3661
barrett-shea <barrettkshea@gmail.com>
2021-07-22 16:36:55 -0400

    Switch to new App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,32 @@
-import React from "react";
-import Dnd from '../Components/Dnd/Dnd'
+import React from "react"
+import Signup from "./Authentication/Signup"
+import { AuthProvider } from "../Contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Profile from "./Authentication/Profile"
+import Login from "./Authentication/Login"
+import PrivateRoute from "./Authentication/PrivateRoute"
+import ForgotPassword from "./Authentication/ForgotPassword"
+import UpdateProfile from "./Authentication/UpdateProfile"
+import Dashboard from './Dnd/Dashboard'
 
-export default function App () {
+export default function App() {
   return (
-    <>
-      <h1 className='m-3'>Kanban Board</h1>
-      <Dnd />
-    </>
-  );
+        <Router>
+          <AuthProvider>
+            <Switch>
+              {/* Kanban */}
+              <PrivateRoute exact path='/' component={Dashboard}/>
+
+              {/* Profile */}
+              <PrivateRoute path="/user" component={Profile} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+
+              {/* Authentication */}
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+  )
 }
